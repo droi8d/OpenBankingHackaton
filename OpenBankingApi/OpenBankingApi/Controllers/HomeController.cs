fe276d18fad31941f0f9d02ff9b630b230c7e929
choosenonee <aprazmowsk@gmail.com>
2018-10-19 20:51:41 +0200

    token creation

--- OpenBankingApi/OpenBankingApi/Controllers/HomeController.cs
+++ OpenBankingApi/OpenBankingApi/Controllers/HomeController.cs
@@ -1,19 +1,15 @@
-﻿using OpenBankingApi.Handlers;
-using System;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using System.Security.Cryptography.X509Certificates;
-using System.IO;
+using Jose;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OpenBankingApi.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly X509Certificate2 _certificate = new X509Certificate2();
         public ActionResult Index()
         {
             return View();
@@ -36,28 +32,27 @@ namespace OpenBankingApi.Controllers
         [HttpPost]
         public async Task<string> Token()
         {
-            var header = new JwtHeader();
-            header.Add("alg", "RS256");
-            header.Add("typ", "JWT");
+            var payload = new Dictionary<string, string>()
+            {
+                { "sub", "team6@bankmillennium.pl" },
+                { "response_code", "Code" },
+                { "redirect_uri", "http://localhost" },
+                { "scope", "[{\"resource\":{\"type\":\"account\",\"accountType\":{\"paymentAccount\":[],\"creditCardAccount\":[]}},\"scopeTimeDuration\":600,\"throttlingPolicy\":\"psd2Regulatory\",\"privilegeList\":{\"ais:accounts\":{\"maxAllowedHistoryLong\":365}},\"scopeGroupType\":\"accountInformationService\"}]" },
+                { "state", "22137C25EE4A3BB48AF76FA7938EB6C340C668DC6204CE5B27BA7BE8433C6F8C" }
+            };
 
-            var payload = new JwtPayload();
-            payload.Add("sub", "aprazmowsk@gmail.com");
-            payload.Add("response_code", "Code");
-            payload.Add("redirect_uri", "http://localhost");
-            payload.Add("scope", "[{\"resource\":{\"type\":\"account\",\"accountType\":{\"paymentAccount\":[],\"creditCardAccount\":[]}},\"scopeTimeDuration\":600,\"throttlingPolicy\":\"psd2Regulatory\",\"privilegeList\":{\"ais:accounts\":{\"maxAllowedHistoryLong\":365}},\"scopeGroupType\":\"accountInformationService\"}]");
-            payload.Add("state", "22137C25EE4A3BB48AF76FA7938EB6C340C668DC6204CE5B27BA7BE8433C6F8C");
+            var path = "D:/cert/bank.millennium.psd2.sandbox.signing.hackathon.team.06.pfx";
+            var cert = RequestSigningHandler.ReadFile(path);
+            _certificate.Import(cert, "millennium", X509KeyStorageFlags.DefaultKeySet);
+            string tokenSigned = JWT.Encode(payload, _certificate.GetRSAPrivateKey(), JwsAlgorithm.RS256);
 
-            var token = new JwtSecurityToken(header: header, payload: payload);
+            var client = new HttpClient();
+            var result = await client.PostAsync("https://bm-devportal-testwebapp03.azurewebsites.net/authorization", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("jwt", tokenSigned)
+            }));
 
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            var aa = new AuthorizationHandler();
-            var response = await aa.SendRequest(tokenString);
-            return response.ToString();
-        }
-
-        private object ReadFile(string path)
-        {
-            throw new NotImplementedException();
+            return await result.Content.ReadAsStringAsync();
         }
     }
 }
