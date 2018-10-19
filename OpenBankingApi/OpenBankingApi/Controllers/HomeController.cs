d013bb31c651381f17699ccad32dbf3911b22ab6
choosenonee <aprazmowsk@gmail.com>
2018-10-19 14:36:42 +0200

    commit

--- OpenBankingApi/OpenBankingApi/Controllers/HomeController.cs
+++ OpenBankingApi/OpenBankingApi/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OpenBankingApi.Handlers;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Claims;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
