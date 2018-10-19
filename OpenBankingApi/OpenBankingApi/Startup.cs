5f7db9d4e83e27c6dcf55177feed9e7a2f54f7b9
droi8d <droi8d@gmail.com>
2018-10-19 12:46:26 +0200

    init2

---
+++ OpenBankingApi/OpenBankingApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenBankingApi.Startup))]
+namespace OpenBankingApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
